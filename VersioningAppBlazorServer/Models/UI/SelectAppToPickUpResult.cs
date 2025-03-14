44aca9ac1cf369f282fbd49b3503af756b9e6e28
rgomolka <rgomolka@jantar.pl>
2025-03-14 13:14:04 +0100

    fix: SizeMd, SizeLg, SizeXl to SizeMM, SizeLG, SizeXL refactor: AddNewCompatibility, AddNewVersion, NewApplication, use AppsPicked / AppsToPick components instead

---
+++ VersioningAppBlazorServer/Models/UI/SelectAppToPickUpResult.cs
@@ -0,0 +1,12 @@
+﻿namespace VersioningAppBlazorServer.Models.UI;
+
+public class SelectAppToPickUpResult
+{
+    public SelectAppToPickUpResult(ApplicationDTO _Application, int _versionId)
+    {
+        Application = _Application;
+        VersionId = _versionId;
+    }
+    public ApplicationDTO Application { get; set; }
+    public int VersionId { get; set; } = -1;
+}
