1bbebb8709d399d51158fc9cce39eae80a3af2e6
rgomolka <rgomolka@jantar.pl>
2025-05-26 15:45:00 +0200

    #refactor: copy AppVersionsView functionality to BaseViewAppWithVersion.razor.razor, use this component in AppVersionView.razor #feat: new page AppsWithLastVersion which shown applications in table, with last version, and production version

---
+++ VersioningAppBlazorServer/Models/UI/DeleteAppVersion.cs
@@ -0,0 +1,7 @@
+﻿namespace VersioningAppBlazorServer.Models.UI;
+
+public class DeleteAppVersion
+{
+    public ApplicationDTO App { get; set; } = new();
+    public AppVersionDTO Version { get; set; } = new();
+}
