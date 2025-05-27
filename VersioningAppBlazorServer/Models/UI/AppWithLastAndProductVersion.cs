38d3cd549d432ae4603189fade1524b3e64aa926
rgomolka <rgomolka@jantar.pl>
2025-05-27 09:34:38 +0200

    #fix: bug LastOrDefault bug when browser back to AppsWithLastVersion page and last version was same as production version, even when last version was x versions bigger

---
+++ VersioningAppBlazorServer/Models/UI/AppWithLastAndProductVersion.cs
@@ -0,0 +1,7 @@
+﻿namespace VersioningAppBlazorServer.Models.UI;
+
+public class AppWithLastAndProductVersion : ApplicationDTO
+{
+    public AppVersionDTO? LastVersion { get; set; } = null;
+    public AppVersionDTO? ProductionVersion { get; set; } = null;
+}
